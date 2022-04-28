fa5370e19f11b4d4c53a3f36889db09be2d66255
Vitor <catchtherainbow33@gmail.com>
2022-04-27 22:29:25 -0300

    feita todas as funcoes

--- app/assets/js/modules/gerar.js
+++ app/assets/js/modules/gerar.js
@@ -2,12 +2,28 @@ export default function initGerar() {
   const QRCode = require('qrcode')
   const text = document.querySelector('#text')
   const button = document.querySelector('#gerar')
+  const save = document.querySelector('#save')
   const img = document.querySelector('.img')
+  const opts = {
+    errorCorrectionLevel: 'H',
+    type: 'image/jpeg',
+    quality: 0.3,
+    margin: 1,
+    color: {
+      dark:"#000",
+      light:"#fff"
+    }
+  }
 
   button.addEventListener('click', () => {
-    QRCode.toDataURL(text.value , function (err, url) {
+    QRCode.toDataURL(text.value , opts ,  (err, url) => {
+      if(err) return err
       img.setAttribute('src', url)
     })
+  })
+
+  save.addEventListener('click', () => {
+    QRCode.toFile(__dirname + `${text.value}.jpeg`,text.value, opts)
   })
 }
 
